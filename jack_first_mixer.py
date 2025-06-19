506d95258e6838a0a3f7182d1115bbd777602650
Dano! <flydanofly@gmail.com>
2025-06-19 13:35:40 -0700

    Now has typing and some minor enhancements

--- jack_first_mixer.py
+++ jack_first_mixer.py
@@ -2,23 +2,14 @@ import signal
 import sys
 import threading
 import time
+import types
+import typing
 from enum import Enum
 
 import jack
-import numpy as np
-import soundfile as sf
+import numpy
+import soundfile
 
-
-class LOOP_INFINITE:
-    # Old trick to have a constant for an out-of-band value
-    # To start my looping strategy is to have `loop` be then number
-    # of loops left, so 0 needs to be a valid value.
-    # This method makes infinite a constant, the alternative is 
-    # the tried and true "a negative number means loop forever"
-    # I'm in the mood to try this in this moment, I may change
-    # it later.
-    pass
- 
 
 class MixerState(Enum):
     INIT = 0
@@ -28,35 +19,37 @@ class MixerState(Enum):
 
 
 class Sound:
-    def __init__(self, filename, volume=1.0, num_loops=0):
-        self.data, self.samplerate = sf.read(filename, dtype='float32')
+    def __init__(self, filename: str, volume: float = 1.0, num_loops: int = 0):
+        self.data: numpy.ndarray
+        self.samplerate: int
+        self.data, self.samplerate = soundfile.read(filename, dtype='float32')
         if len(self.data.shape) == 1:
-            self.data = self.data[:, np.newaxis]  # mono to 2D
-        self.position = 0
-        self.channels = self.data.shape[1]
-        self.volume = volume
-        self.loops = num_loops
+            self.data = self.data[:, numpy.newaxis]  # mono to 2D
+        self.position: int = 0
+        self.channels: int = self.data.shape[1]
+        self.volume: float = volume
+        self.loops: int = num_loops
         # TODO: if I get into adding reltime effects, make this an effect
-        self.fade_out_active = False
-        self.fade_out_frames_remaining = 0
-        self.fade_out_step = 0.0
+        self.fade_out_active: bool = False
+        self.fade_out_frames_remaining: int = 0
+        self.fade_out_step: float = 0.0
         self.fade_out_comelete: bool = False
 
-    def is_done(self):
-        return (
-            not self.loops and (
-                self.fade_out_comelete or
-                self.position >= len(self.data) and
-                not self.fade_out_active
-            )
-        )
+    def is_done(self) -> bool:
+        if self.loops:
+            return False
+        if self.fade_out_comelete:
+            return True
+        if self.fade_out_active:
+            return False
+        return self.position >= len(self.data)
 
     def start_fade_out(self, duration_sec: float, samplerate: int) -> None:
         self.fade_out_active = True
         self.fade_out_frames_remaining = int(duration_sec * samplerate)
         self.fade_out_step = self.volume / max(1, self.fade_out_frames_remaining)
 
-    def mix_into(self, output_buffers, channel_map):
+    def mix_into(self, output_buffers: list[numpy.ndarray], channel_map: list[int]) -> None:
         frames = len(output_buffers[0])
         for i, target_channel in enumerate(channel_map):
             if target_channel >= len(output_buffers):
@@ -64,23 +57,24 @@ class Sound:
             buf = output_buffers[target_channel]
             for f in range(frames):
                 if self.position >= len(self.data):
-                    if self.loops:
+                    if self.loops > 0:
                         self.position = 0
-                        if self.loops is not LOOP_INFINITE:
+                        if self.loops < 0:
                             self.loops -= 0
                     else:
                         break
                 if self.volume >= 0.95:
                     sample = self.data[self.position, i % self.channels] * self.volume
+                    # sample = self.data[self.position, i % self.channels]
                 else:
-                    sample = self.data[self.position, i % self.channels]
+                    sample = self.data[self.position, i % self.channels] * self.volume
                 sample = self.data[self.position, i % self.channels] * self.volume
                 buf[f] += sample
                 self.position += 1
                 if self.fade_out_active:
-                    self.volume -= self.fade_out_step
+                    self.volume = max(0.0, self.volume - self.fade_out_step)
                     self.fade_out_frames_remaining -= 1
-                    if self.fade_out_frames_remaining <= 0:
+                    if self.fade_out_frames_remaining <= 0 or self.volume <= 0.0:
                         self.volume = 0.0
                         self.fade_out_active = False
                         self.fade_out_comelete = True
@@ -88,22 +82,25 @@ class Sound:
 
 
 class JackMixer:
-    def __init__(self, name="jack_mixer", num_channels=8):
-        self.client = jack.Client(name)
-        self.num_channels = num_channels
-        self.outports = [self.client.outports.register(f"out_{i}") for i in range(num_channels)]
-        self.active_sounds = []
-        self.lock = threading.Lock()
-        self.state = MixerState.INIT
+    def __init__(self, name: str = "jack_mixer", num_channels: int = 8):
+        self.client: jack.Client = jack.Client(name)
+        self.num_channels: int = num_channels
+        self.outports: list[jack.OwnPort] = [typing.cast("jack.OwnPort", self.client.outports.register(f"out_{i}")) for i in range(num_channels)]
+        self.active_sounds: list[tuple[Sound, list[int]]] = []
+        self.lock: threading.Lock = threading.Lock()
+        self.state: MixerState = MixerState.INIT
+        self.shutdown_called: bool = False
         self.client.set_process_callback(self.process)
 
-    def process(self, frames):
+    def process(self, _: int):
+        # the parameter is the number of frames to process
         if self.state != MixerState.STARTED:
             return
 
         # Create empty output buffers
+
         output_buffers = [
-            np.frombuffer(port.get_buffer(), dtype=np.float32)
+            numpy.frombuffer(port.get_buffer(), dtype=numpy.float32)
             for port in self.outports
         ]
         for buf in output_buffers:
@@ -134,8 +131,9 @@ class JackMixer:
                 self.client.connect(port, system_out[i].name)
 
     def shutdown(self):
-        if self.state in [MixerState.SHUTDOWN, MixerState.INIT]:
+        if self.shutdown_called or self.state in [MixerState.SHUTDOWN, MixerState.INIT]:
             return
+        self.shutdown_called = True
         self.state = MixerState.SHUTDOWN
         print("Shutting down JACK mixer...")
         self.client.deactivate()
@@ -165,11 +163,16 @@ def main():
     snd2 = Sound("LRMonoPhase4_8ch.wav")
 
     mixer.play(snd1, [0])       # Play to channel 0
-    time.sleep(5.0)
+    time.sleep(1.0)
     mixer.play(snd2, [1])    # Play stereo sound to channels 2 and 3
     # mixer.play(snd2, [2, 3])    # Play stereo sound to channels 2 and 3
 
-    def shutdown_handler(sig, frame):
+    def shutdown_handler(*_) -> None:
+        # handler(signal_number: int, frame: types.FrameType|None)
+        # The handler is called with two arguments: the signal number and the
+        # current stack frame (None or a frame object; for a description of
+        # frame objects, see the description in the type hierarchy or see the
+        # attribute descriptions in the inspect module).
         print("\nSignal received, shutting down...")
         mixer.stop_all(fade_duration=1.0)
         # time.sleep(1.2)
@@ -193,11 +196,7 @@ def main():
     except KeyboardInterrupt:
         print("keyboardinterrupt received")
 
-    # print("giving it a little extra time for a clean finish")
-    # time.sleep(3)
     mixer.shutdown()
-    print("and just a little more...")
-    time.sleep(0.5)
     print("that's it")
 
 if __name__ == "__main__":
